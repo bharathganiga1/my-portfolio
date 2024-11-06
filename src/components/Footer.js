7de4f6167b2ccbacd2365c3503a5231b12eae723
Bharath <bharathganiga2002@gmail.com>
2024-11-06 09:13:59 +0530

    footer css changed little bit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,20 +8,20 @@ function Footer() {
     useEffect(() => { const handleScroll = () => { const footer = document.querySelector('.footer'); footer.style.top = `${window.innerHeight / 2 + window.scrollY-190}px`; }; window.addEventListener('scroll', handleScroll); handleScroll(); return () => { window.removeEventListener('scroll', handleScroll); }; }, []);
 
     return (
-        <div className="footer">
-            <div className="social-icon">
-                <a href="https://www.linkedin.com/in/bharath9901/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-            </div>
-            <div className="social-icon">
-                <a href="https://github.com/bharathganiga1" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-            </div>
-            <div className="social-icon">
-                <a href="https://www.instagram.com/__im_bharathh_/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-            </div>
-            <div className="social-icon">
-                <a href="mailto:bharathganiga2002@gmail.com"><FaEnvelope /></a>
-            </div>
-        </div>
+        <ul class="footer">
+          <li className='social-icon'>
+          <a href="https://www.linkedin.com/in/bharath9901/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          </li>
+          <li className='social-icon'>
+          <a href="https://github.com/bharathganiga1" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+          </li>
+          <li className='social-icon'>
+            <a href="https://www.instagram.com/__im_bharathh_/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          </li>
+          <li className='social-icon'>
+            <a href="mailto:bharathganiga2002@gmail.com"><FaEnvelope /></a>
+          </li>
+        </ul>
     );
 }
 
